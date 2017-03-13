c60a26397435d67e26fab1b48f49acbeb70e1952
chenc18 <chenc18@mcmaster.ca>
2017-03-13 12:42:38 -0400

    hola

--- src/GitProjectTest.java
+++ src/GitProjectTest.java
@@ -3,5 +3,7 @@ public class GitProjectTest {
 	public static void main(String[] args){
 		System.out.println("Hello git test test");
 		System.out.println("Git test 2");
+		String fuck = "me";
 	}
+	
 }
