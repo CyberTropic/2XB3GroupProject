9ee6552cc526649c867db6660d65301a991956b7
chenc18 <chenc18@mcmaster.ca>
2017-03-19 18:23:02 -0400

    HOLA AMIGOS PROCESSING EST FUEGO *FUEGO EMOJI *EXTRA PICANTE

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,9 +1,50 @@
+import processing.core.PApplet;
+import processing.core.*;
 
-public class GitProjectTest {
+public class UserInterface extends PApplet{
+	
 	public static void main(String[] args){
-		System.out.println("Hello git test test");
-		System.out.println("Git test 2");
-		String fuck = "me";
+		PApplet.main("UserInterface");
+		
 	}
 	
+	public PImage vLAD;
+	public Boolean searching = false;
+	
+	public void settings(){
+		size(1000,1000);
+	}
+
+    public void setup(){
+    	background(255);
+    }
+
+    public void draw(){
+    	while (true){
+    		searchBar();
+    		fill(255);
+    		background(255);
+    	}
+    }
+    
+    public void searchBar(){
+    	while (searching){
+    		String term = "";
+        	stroke(10);
+        	fill(0);
+        	rect(50,100, 900, 50);
+    	}
+
+
+    	
+    }
+    
+    public void mouseClicked(){
+    	if (((mouseX>50)&&(mouseX<950))&&((mouseY>100)&&(mouseY<150))){
+    		searching = true;
+    	}
+    	else{
+    		searching = false;
+    	}
+    }
 }
