5f23d5663b65566800dbf64b8025be1829350920
Vladysav Falach <falachv@mcmaster.ca>
2017-03-21 19:05:41 -0400

    Working on complaint scanner; made a state adjacency list

--- src/ComplaintScanner.java
+++ src/ComplaintScanner.java
@@ -1,15 +1,23 @@
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.Reader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
 import com.opencsv.CSVReader;
 
 public class ComplaintScanner {
+	
+	static ArrayList<String> stateIndex = new ArrayList<>();
+	static ArrayList<String[]> stateAdj = new ArrayList<>();
+	static ArrayList<Complaint> allComplaints = new ArrayList<>();
+	
 
 	public static void createComplaints() {
 		// Initialize data reader
-		ArrayList<Complaint> allComplaints = new ArrayList<>();
 		String folder = "resources";
 		// File f = new File(folder + "/CC_smallDatabase.csv");
 		// Reader inFile = null;
@@ -45,7 +53,7 @@ public class ComplaintScanner {
 				String zipCode = tokens[9];
 				String state = tokens[8];
 				Complaint readerComplaint = new Complaint(complaintID, product, company, zipCode, state);
-				System.out.println(complaintID + ";" + product + ";"+company+";"+zipCode+";"+state);
+				System.out.println(complaintID + ";" + product + ";" + company + ";" + zipCode + ";" + state);
 				allComplaints.add(readerComplaint);
 			}
 		} catch (IOException e) {
@@ -54,7 +62,43 @@ public class ComplaintScanner {
 		}
 	}
 
+	public static void stateScanner() {
+		
+
+		String folder = "resources";
+		File f = new File(folder + "/StateAdjacency.txt");
+		Scanner inFile = null;
+		
+		try {
+			inFile = new Scanner(f);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		while (inFile.hasNextLine()) {
+			String[] nl = inFile.nextLine().split(",");
+			stateIndex.add(nl[0]);
+			String[] adj = new String[0];
+			if (nl.length > 1) {
+				adj = Arrays.copyOfRange(nl, 1, nl.length);
+			}
+			stateAdj.add(adj);
+		}
+
+		for (int i = 0; i < stateIndex.size(); i++) {
+			System.out.println(stateIndex.get(i) + " adjacent to:");
+			if (stateAdj.get(i).length > 0){
+				System.out.println(Arrays.toString(stateAdj.get(i)));
+			} else {
+				System.out.println("none");
+			}
+		}
+
+	}
+
 	public static void main(String[] args) {
+		ComplaintScanner.stateScanner();
 		ComplaintScanner.createComplaints();
 	}
 
